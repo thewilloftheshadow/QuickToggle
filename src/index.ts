cf35d1296927592295de3cf7bd6d722a42edbd01
Shadow <hi@shadowing.dev>
2023-02-27 21:08:14 -0600

    remove event listener on stop

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,56 @@
-import { Injector, Logger, plugins, themes } from "replugged";
+import { Logger, plugins, themes } from "replugged";
 
-const inject = new Injector();
 const logger = new Logger("Plugin", "QuickToggle");
 
 let themesDisabled = false;
 let pluginsDisabled = false;
 let isRunning = false;
 
+async function checkKeyDown(e: KeyboardEvent): Promise<void> {
+  if (isRunning) return;
+  isRunning = true;
+  if (e.repeat) return;
+  switch (e.key) {
+    case "F6": // toggle themes
+      if (themesDisabled) {
+        themes.loadAll();
+        logger.log("Loading all themes");
+        themesDisabled = false;
+      } else {
+        themes.unloadAll();
+        logger.log("Unloading all themes");
+        themesDisabled = true;
+      }
+      break;
+
+    case "F7": // toggle plugins
+      if (pluginsDisabled) {
+        await plugins.startAll();
+        logger.log("Starting all plugins");
+        pluginsDisabled = false;
+      } else {
+        const disabled = plugins.getDisabled();
+
+        for (const plugin of plugins.plugins) {
+          const id = plugin[0];
+          const { name } = plugin[1].manifest;
+          if (disabled.includes(plugin[0])) continue;
+          if (plugin[0] === "dev.shadow.quicktoggle") continue;
+          await plugins.stop(id).catch(() => {
+            logger.log(`${name} is already stopped or failed to stop`);
+          });
+          console.log(`${name} is stopped`);
+        }
+        pluginsDisabled = true;
+      }
+  }
+  isRunning = false;
+}
+
 export function start(): void {
-  document.addEventListener("keydown", async (e: KeyboardEvent) => {
-    if (isRunning) return;
-    isRunning = true;
-    if (e.repeat) return;
-    switch (e.key) {
-      case "F6": // toggle themes
-        if (themesDisabled) {
-          themes.loadAll();
-          logger.log("Loading all themes");
-          themesDisabled = false;
-        } else {
-          themes.unloadAll();
-          logger.log("Unloading all themes");
-          themesDisabled = true;
-        }
-        break;
-
-      case "F7": // toggle plugins
-        if (pluginsDisabled) {
-          await plugins.startAll();
-          logger.log("Starting all plugins");
-          pluginsDisabled = false;
-        } else {
-          const disabled = plugins.getDisabled();
-
-          for (const plugin of plugins.plugins) {
-            const id = plugin[0];
-            const { name } = plugin[1].manifest;
-            if (disabled.includes(plugin[0])) continue;
-            if (plugin[0] === "dev.shadow.quicktoggle") continue;
-            await plugins.stop(id).catch(() => {
-              logger.log(`${name} is already stopped or failed to stop`);
-            });
-            console.log(`${name} is stopped`);
-          }
-          pluginsDisabled = true;
-        }
-    }
-    isRunning = false;
-  });
+  document.addEventListener("keydown", checkKeyDown);
 }
 
 export function stop(): void {
-  inject.uninjectAll();
+  document.removeEventListener("keydown", checkKeyDown);
 }
